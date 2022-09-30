932f33ec32922904f52ad5881ec450ac56bf82be
thawtar <mr.thaw.tar1990@gmail.com>
2022-09-30 19:59:59 +0900

    1

--- counter.c
+++ counter.c
@@ -3,6 +3,13 @@
 
 int main()
 {
-return 0;
+    int N = 12;
+    for(int i=0;i<N;i++)
+    {
+        printf("%d\n",(N-i));
+        if((N-i)%5==0)
+            printf("Beep\n");
+    }
+    return 0;
 }
 
